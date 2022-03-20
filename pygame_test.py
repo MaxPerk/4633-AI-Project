6809710eebaf575fddf776373cdf0d1c163c3cc2
Maxwell <mperk1000@gmail.com>
2022-03-20 08:49:39 -0500

    Maze gui

---
+++ pygame_test.py
@@ -0,0 +1,226 @@
+import pygame
+from pygame.locals import *
+import numpy as np
+import random as rd
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (127, 127, 127)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+def carve_maze(grid:np.ndarray, size:int) -> np.ndarray:
+    output_grid = np.empty([size*3, size*3],dtype=int)
+    output_grid[:] = 0
+    
+    i = 0
+    j = 0
+    
+    
+    while i < size:
+        previous_l = []
+        w = i*3 + 1
+        while j < size:
+            k = j*3 + 1
+            toss = grid[i,j]
+            output_grid[w,k] = -1
+            if toss == 0 and k+2 < size*3:
+                output_grid[w,k+1] = -1
+                output_grid[w,k+2] = -1
+                previous_l.append(j)
+            if toss == 1:
+                # it's impossible to carve outside after preprocessing
+                # look back, choose a random cell
+                if grid[i,j-1] == 0:
+                    # reaching from 0
+                    # mandatory to be sure that previous_l has at least one element
+                    # if we are coming from a list of previous cells, choose one and...
+                    r = rd.choice(previous_l)
+                    k = r * 3 + 1
+                   
+                # ...just carve north
+                # this just carve north if this is the first element of the row (1 element loop)
+                output_grid[w-1,k] = -1
+                output_grid[w-2,k] = -1
+                previous_l = []
+            
+            j += 1
+            
+        i += 1
+        j = 0
+        
+    return output_grid
+
+def preprocess_grid(grid:np.ndarray, size:int) -> np.ndarray:
+    # fix first row and last column to avoid digging outside the maze external borders
+    first_row = grid[0]
+    first_row[first_row == 1] = 0
+    grid[0] = first_row
+    for i in range(1,size):
+        grid[i,size-1] = 1
+    return grid
+
+def main(val):
+    n=1
+    p=0.5
+    size=val
+
+    # 1 (head) N, 0 (tail) E
+    # np.random.seed(42)
+    grid = np.random.binomial(n,p, size=(size,size))
+    # print(grid)
+
+    processed_grid = preprocess_grid(grid, size)
+    print(processed_grid)
+
+    output = carve_maze(processed_grid, size)
+    print(output)
+
+    return output
+
+class Grid:
+    def __init__(self,row_length, col_length):
+        self.matrix = main(row_length)
+
+    def setIndex(self, row, col, value):
+        self.matrix[row, col] = value
+
+    def getIndex(self, row, col):
+        return self.matrix[row, col]
+
+    def getRowLen(self):
+        return self.matrix.shape[0]-1
+    
+    def getColLen(self):
+        return self.matrix.shape[1]-1
+
+class Maze:
+   def __init__(self, screen_w, screen_h):
+
+    # screen dimensions
+    self.screen_w = screen_w
+    self.screen_h = screen_h
+
+    # cell dimensions
+    self.cell_width = 20
+    self.cell_height = 20
+    self.cell_margin = 5
+
+    # init game and set screen dims
+    pygame.init()
+
+    # matrix for cells
+    self.grid = Grid(
+        self.screen_w // (self.cell_width+self.cell_margin), 
+        self.screen_w // (self.cell_height+self.cell_margin) 
+        )
+
+    self.screen_w = screen_w * 3 - 10
+    self.screen_h = screen_h * 3 - 10
+    self.screen = pygame.display.set_mode((self.screen_w, self.screen_h))
+
+    # setup variables
+    self.screen.fill(WHITE)
+    self.background = BLACK
+    self.running = True
+    self.clock = pygame.time.Clock()
+    self.drag=False
+    self.clear = False
+
+   def run(self):
+        while self.running:
+            for event in pygame.event.get():
+
+                # Exit window
+                if event.type == pygame.QUIT:
+                    self.running = False
+                
+                # Background color keys
+                elif event.type == KEYDOWN:
+                    if event.key == K_g:
+                        self.background = GREEN
+                    elif event.key == K_b:
+                        self.background = BLUE
+                    elif event.key == K_r:
+                        self.background = RED
+                    elif event.key == K_w:
+                        self.background = WHITE
+                    elif event.key == K_k:
+                        self.background = BLACK
+
+                # Unclick stops events
+                elif event.type == MOUSEBUTTONUP:
+                    self.drag=False
+                    self.clear=False
+                
+                # Erase grid colors
+                elif self.drag and self.clear:
+                    x_pos, y_pos = pygame.mouse.get_pos()
+
+                    x_pos = x_pos // (self.cell_width+self.cell_margin)
+                    y_pos = y_pos // (self.cell_height+self.cell_margin)
+
+                    # if self.grid.getIndex(y_pos, x_pos) == 1:
+                    self.grid.setIndex(y_pos, x_pos, 0)
+                
+                # Color grid cells
+                elif self.drag:
+                    x_pos, y_pos = pygame.mouse.get_pos()
+
+                    x_pos = x_pos // (self.cell_width+self.cell_margin)
+                    y_pos = y_pos // (self.cell_height+self.cell_margin)
+
+                    if self.grid.getIndex(y_pos, x_pos) == 0:
+                        self.grid.setIndex(y_pos, x_pos, 1)
+                
+                # Color single grid cell
+                elif event.type == MOUSEBUTTONDOWN:
+                    x_pos, y_pos = pygame.mouse.get_pos()
+
+                    x_pos = x_pos // (self.cell_width+self.cell_margin)
+                    y_pos = y_pos // (self.cell_height+self.cell_margin)
+
+                    if self.grid.getIndex(y_pos, x_pos):
+                        if self.grid.getIndex(y_pos, x_pos) == -1:
+                            pass
+                        else:
+                            self.grid.setIndex(y_pos, x_pos, 0)
+                        self.clear=True
+                    else:
+                        self.grid.setIndex(y_pos, x_pos, 1)
+
+                    self.drag = True
+
+            # fill background color
+            self.screen.fill(self.background)
+
+            # create grid
+            for i in range(self.screen_h // (self.cell_height+self.cell_margin)):
+                for j in range(self.screen_w // (self.cell_width+self.cell_margin)):
+                    y_pos = i * (self.cell_height+self.cell_margin) +self.cell_margin
+                    x_pos = j * (self.cell_width+self.cell_margin) + self.cell_margin
+
+            # Black if cell is 1 white otherwise
+                    if self.grid.getIndex(i, j) == -1:
+                        pygame.draw.rect(self.screen, BLACK, (x_pos,y_pos, self.cell_width, self.cell_height))
+                    elif self.grid.getIndex(i, j) == 0:
+                        pygame.draw.rect(self.screen, WHITE, (x_pos,y_pos, self.cell_width, self.cell_height))
+                    else:
+                        pygame.draw.rect(self.screen, GREEN, (x_pos,y_pos, self.cell_width, self.cell_height))
+
+                    if (i == 0 or j == 0) or (i == self.grid.getColLen() or j == self.grid.getRowLen()):
+                        pygame.draw.rect(self.screen, GRAY, (x_pos,y_pos, self.cell_width, self.cell_height))
+            # set fps
+            self.clock.tick(20)
+
+            # display changes
+            pygame.display.flip()
+
+        #quit game
+        pygame.quit()
+
+if __name__ == "__main__":
+    maze = Maze(255, 255)
+    maze.run()
